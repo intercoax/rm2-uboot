1bf1091099e7a33ff3df96a01d72b4d80da51ae9
licy <licy.li@myirtech.com>
2020-10-27 03:11:51 -0700

    support cubeprog 2.5

--- drivers/dfu/dfu.c
+++ drivers/dfu/dfu.c
@@ -317,7 +317,7 @@ int dfu_write(struct dfu_entity *dfu, void *buf, int size, int blk_seq_num)
 	ret = dfu_transaction_initiate(dfu, false);
 	if (ret < 0)
 		return ret;
-	blk_seq_num -= 2;
+	//blk_seq_num -= 2;
 	if (dfu->i_blk_seq_num != blk_seq_num) {
 		printf("%s: Wrong sequence number! [%d] [%d]\n",
 		       __func__, dfu->i_blk_seq_num, blk_seq_num);
